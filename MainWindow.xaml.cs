8ac4abe0885ac4064217d9b9735fd9c0f5011b5b
dz-paji <i@paji.uk>
2024-06-01 16:48:56 +0100

    bug fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
 using System.Diagnostics;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Web.WebView2.Core;
-using Microsoft.Web.WebView2.WinForms;
 using Discord;
 using System.Threading;
+using System.IO;
 
 namespace BiliCord
 {
@@ -27,12 +17,24 @@ namespace BiliCord
     public partial class MainWindow : Window
     {
         Stack<Uri> history = new Stack<Uri>();
+        Discord.ActivityManager activityManager;
 
         public MainWindow()
         {
-            InitializeComponent();
-            InitialiseWebView();
-            InitialiseDiscord();
+            try
+            {
+                InitializeComponent();
+                InitialiseWebView();
+                InitialiseDiscord();
+            }
+            catch (Exception e)
+            {
+                StreamWriter sw = new StreamWriter("error.txt");
+                sw.WriteLine(e.Message);
+                sw.WriteLine(e.StackTrace);
+                sw.Close();
+            }
+
         }
 
         private void InitialiseDiscord()
@@ -40,7 +42,7 @@ namespace BiliCord
             var clientID = Environment.GetEnvironmentVariable("DISCORD_CLIENT_ID");
             if (clientID == null)
             {
-                clientID = "418559331265675294";
+                clientID = "1245145932501356595";
             }
             var discord = new Discord.Discord(Int64.Parse(clientID), (UInt64)Discord.CreateFlags.Default);
             discord.RunCallbacks();
@@ -58,6 +60,7 @@ namespace BiliCord
 
             var activityManager = discord.GetActivityManager();
             var lobbyManager = discord.GetLobbyManager();
+            this.activityManager = activityManager;
 
             activityManager.RegisterCommand();
             var activity = new Discord.Activity
@@ -83,6 +86,21 @@ namespace BiliCord
             discordThread.IsBackground = true;
             discordThread.Start();
 
+        }
+
+        private void updateActivity(Discord.Activity activity)
+        {
+            this.activityManager.UpdateActivity(activity, (res) =>
+            {
+                if (res == Discord.Result.Ok)
+                {
+                    Trace.WriteLine("Discord Rich Presence Updated");
+                }
+                else
+                {
+                    Trace.WriteLine("Discord Rich Presence Update Failed");
+                }
+            });
         }
 
         private void DiscordLoop(Discord.Discord discord, LobbyManager lobbyManager)
@@ -117,12 +135,14 @@ namespace BiliCord
             String url = webView.Source.ToString();
             webView.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
             webView.CoreWebView2.SourceChanged += CoreWebView2_SourceChanged;
-
-            // subscribe to the NavigationStarting event
-            webView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
-            webView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
+            // listen for title change
+            webView.CoreWebView2.DocumentTitleChanged += CoreWebView2_DocumentTitleChanged;
         }
 
+        private void CoreWebView2_DocumentTitleChanged(object? sender, object e)
+        {
+            Update_Activity();
+        }
 
         private void CoreWebView2_NewWindowRequested(object sender,
             CoreWebView2NewWindowRequestedEventArgs e)
@@ -133,19 +153,57 @@ namespace BiliCord
 
         private void CoreWebView2_SourceChanged(object sender, CoreWebView2SourceChangedEventArgs e)
         {
+            Update_Activity();
+        }
+
+        private void Update_Activity()
+        {
             String url = webView.Source.ToString();
-            history.Push(new Uri(url));
-        }
+            String title = webView.CoreWebView2.DocumentTitle;
+            String BV = "";
+            if (title == "")
+            {
+                title = "Watching Bilibili";
+            }
+            // conver string title to utf-8 encoding
+            byte[] bytes = Encoding.Default.GetBytes(title);
+            var new_title = Encoding.UTF8.GetString(bytes);
+            new_title = new_title.Split("_哔哩哔哩")[0];
 
-        private void CoreWebView2_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
-        {
-            // Log or handle navigation starting if needed
-        }
+            // Find the index of the prefix
+            int startIndex = url.IndexOf("BV");
 
-        private void CoreWebView2_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
-        {
-            // Check and update CanGoBack state when navigation is completed
-            CommandManager.InvalidateRequerySuggested();
+            // If the prefix is found, extract the substring starting from the prefix
+            if (startIndex != -1)
+            {
+                BV = url.Substring(startIndex);
+                BV = BV.Split("/?")[0];
+            }
+
+            Trace.WriteLine("Title: " + new_title);
+            var activity = new Discord.Activity
+            {
+                State = BV,
+                Details = new_title,
+            };
+            updateActivity(activity);
+
+            // push if not bili rewrite url
+            if (history.Count == 0)
+            {
+                history.Push(new Uri(url));
+
+            }
+            else if (history.Peek().ToString().Split("/?")[0].StartsWith(url.Split("/?")[0]) == false)
+            {
+                history.Push(new Uri(url));
+            }
+
+            // print out stack
+            foreach (Uri uri in history)
+            {
+                Trace.WriteLine(uri);
+            }
         }
 
         void BrowseBack_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -165,4 +223,6 @@ namespace BiliCord
         }
 
     }
+
+
 }
